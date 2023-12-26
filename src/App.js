0a17f61a4f35bdba260e9453b50a9d798cc29f3e
esma <esma.ea.aksoy@gmail.com>
2023-12-26 14:42:14 +0300

    APP

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
-import Footer from "./Components/Footer"
-import Navbar from "./Components/Navbar";
-import ProductCard from "./Components/ProductCard";
-import Login from "./Pages/Login";
+import AuthProvider from "./context/AuthProvider";
+import ProductProvider from "./context/ProductProvider";
+import AppRouter from "./router/AppRouter";
 function App() {
   return (
+<AuthProvider>
+<ProductProvider>
+  <AppRouter/>
+</ProductProvider>
+</AuthProvider>
 
   );
 }
